5cd6a4343c29375f82ccb2ecbf2e7cc339836411
Otakenne <samuel.egbeo@gmail.com>
2021-10-26 22:16:19 +0100

    Styled the views

---
+++ app/src/main/java/com/celerii/playio/Fragments/ArtistDetailFragment.java
@@ -0,0 +1,28 @@
+package com.celerii.playio.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.celerii.playio.R;
+
+/**
+ *
+ */
+public class ArtistDetailFragment extends Fragment {
+
+    public ArtistDetailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_artist_detail, container, false);
+    }
+}
---
+++ app/src/main/java/com/celerii/playio/Fragments/TrackDetailFragment.java
@@ -0,0 +1,28 @@
+package com.celerii.playio.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.celerii.playio.R;
+
+/**
+ *
+ */
+public class TrackDetailFragment extends Fragment {
+
+    public TrackDetailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_track_detail, container, false);
+    }
+}
